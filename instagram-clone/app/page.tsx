c02340bc0c82405c76bef6584971b79a0eee2a4b
shka5 <shka5.dev@gmail.com>
2024-08-25 16:42:11 +0900

    2024-08-25 ENH Add - 환경세팅 구현

---
+++ instagram-clone/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export const metadata = {
+  title: "Inflearngram",
+  description: "Instagram clone project",
+};
+
+export default function Home() {
+  return <main className="">Inflearngram</main>;
+}
